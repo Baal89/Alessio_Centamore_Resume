236cad752b3a07f735243e611672e87fb5d97e92
Baal89 <efestos89@gmail.com>
2020-07-12 18:10:47 +0000

    wrap jQuery in a ready function

--- static/js/resume.js
+++ static/js/resume.js
@@ -26,7 +26,8 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
+  }
+
 
 //scroll back to the top
 mybutton = document.getElementById("myBtn");
@@ -37,34 +38,38 @@ function topFunction() {
 }
 
 //jQuery to change the color of the navbar on scroll. In this case I preferred jQuery as it works with all the browsers and is less verbose
-var myNav = $("#myBtn");
-var myResume = $("#myResume")
 
-$(window).on('scroll', function() {
-  if ($(window).scrollTop() >= 643 && $(window).scrollTop() < 1286 || $(window).scrollTop() > 1929)  {
-    myNav.addClass("scroll");
-    myResume.addClass("scroll-resume")
-  } else {
-    myNav.removeClass("scroll");
-    myResume.removeClass("scroll-resume")
-  }
-});
+$(document).ready(function(){
 
-// stack everflow code - I needed this to prevent the modal to appear on empty form
-$('#form').on('submit',function(event){
-  event.preventDefault()
-  var name = $('#name').val();
-  if(name == "" && email == "" && subject == "" && projectsummary == "") {
-    return false;
-  }else{
-    $('#myModal').modal('show');
-        }
+    var myNav = $("#myBtn");
+    var myResume = $("#myResume")
+
+    $(window).on('scroll', function() {
+    if ($(window).scrollTop() >= 643 && $(window).scrollTop() < 1286 || $(window).scrollTop() > 1929)  {
+        myNav.addClass("scroll");
+        myResume.addClass("scroll-resume")
+    } else {
+        myNav.removeClass("scroll");
+        myResume.removeClass("scroll-resume")
+    }
     });
-  
-if ($(window).width() < 1200) {
-    $('#nav-bar').removeClass('fixed-top');
-} 
 
-if ($(window).width() < 600) {
-    $('#footer-button').removeClass('d-flex')
-}
+    // stack everflow code - I needed this to prevent the modal to appear on empty form
+    $('#form').on('submit',function(event){
+    event.preventDefault()
+    var name = $('#name').val();
+    if(name == "" && email == "" && subject == "" && projectsummary == "") {
+        return false;
+    }else{
+        $('#myModal').modal('show');
+            }
+        });
+    
+    if ($(window).width() < 1200) {
+        $('#nav-bar').removeClass('fixed-top');
+    } 
+
+    if ($(window).width() < 600) {
+        $('#footer-button').removeClass('d-flex')
+    }
+})
